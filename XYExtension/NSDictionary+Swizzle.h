e66c8f0fe2150d8e82551d234f5c915336a7d7dd
chenqing512 <chenqing512@sina.com>
2017-10-05 13:22:15 +0800

    添加文件

---
+++ XYExtension/NSDictionary+Swizzle.h
@@ -0,0 +1,19 @@
+//
+//  NSDictionary+Swizzle.h
+//  Health
+//
+//  Created by System Administrator on 2017/5/17.
+//  Copyright © 2017年 KevinShi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (Swizzle)
+
+@end
+
+
+
+@interface NSMutableDictionary (Swizzle)
+
+@end
